adf65d3eb0e6a450c69c94afbbd1aa890cf39c79
brianpoanlin <brian.poan.lin@gmail.com>
2015-09-17 21:26:11 -0700

    Counter with simple Swipe Gestures and UIButtons

---
+++ Counter/Counter/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Brian Lin on 9/17/15.
+//  Copyright (c) 2015 Brian Lin. All rights reserved.
+//
+
+import UIKit
+
+var amount=0;
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var AmountLabel:UILabel?
+    
+    @IBAction func swipedUp(sender:UISwipeGestureRecognizer){
+        amount+=1;
+        AmountLabel?.text=String(amount);
+    }
+    
+    @IBAction func swipedDown(sender:UISwipeGestureRecognizer){
+        if(amount>=1){
+            amount-=1;
+        }
+        AmountLabel?.text=String(amount);
+    }
+    
+    
+    @IBAction func pressedPlus(sender:UIButton){
+        amount+=1;
+        AmountLabel?.text=String(amount);
+    }
+    
+    @IBAction func pressedMinus(sender:UIButton){
+        if(amount>=1){
+            amount-=1;
+        }
+        AmountLabel?.text=String(amount);
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        AmountLabel?.text=String(amount);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
